239629a52e467d92a9cff0e7ea90a2b9c6ac9669
meraf00 <meraf000@gmail.com>
2022-06-28 17:40:44 +0300

    Commit

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range (1, 100):
+    for i in range (1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz ", end="")
         elif i % 3 == 0:
@@ -8,5 +8,4 @@ def fizzbuzz():
         elif i % 5 == 0:
             print("Buzz ", end="")
         else:
-            print("{0} ".format(i), end="")
-    print("Buzz")
+            print("{0} ".format(i), end="")    
