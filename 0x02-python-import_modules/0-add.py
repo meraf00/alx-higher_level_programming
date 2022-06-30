4d6f6e17546b3740502404c6d16d42c4034f1457
meraf00 <meraf000@gmail.com>
2022-06-30 17:42:07 +0300

    Commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0.add as add
 if __name__ == "__main__":
+    import add_0.add as add
     a = 1
     b = 2    
     print("{0:d} + {1:d} = {2:d}".format(a, b, add(a, b)))
