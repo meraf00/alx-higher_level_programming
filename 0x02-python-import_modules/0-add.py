009973f15abc78f86f38663136249eff73de2cbe
meraf00 <meraf000@gmail.com>
2022-06-30 17:49:04 +0300

    Commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """Prints the sum of 1 and 2"""
-    import add_0.add as add
+    import add_0 as add
 
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add.add(a, b)))
